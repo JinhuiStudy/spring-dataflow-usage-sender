3ab5b4082c176f0f28e289c7bc75e893d11f8442
JinhuiStudy <kpo444@naver.com>
2022-05-24 15:17:23 +0900

    Initial commit

---
+++ src/main/java/io/spring/dataflow/springdataflow/SpringDataflowApplication.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.springdataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/spring/dataflow/springdataflow/SpringDataflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.springdataflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
