02afbf65dec36045f7534252562b41b1aac81c89
JinhuiStudy <kpo444@naver.com>
2022-05-25 16:27:14 +0900

    sender error edit

--- src/test/java/io/spring/dataflow/springdataflow/SpringDataflowApplicationTests.java
+++ src/test/java/io/spring/dataflow/springdataflow/SpringDataflowApplicationTests.java
@@ -1,13 +1,44 @@
 package io.spring.dataflow.springdataflow;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.converter.CompositeMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringDataflowApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testUsageDetailSender() {
+        try (ConfigurableApplicationContext context = new SpringApplicationBuilder(
+                TestChannelBinderConfiguration
+                        .getCompleteConfiguration(SpringDataflowApplication.class))
+                .web(WebApplicationType.NONE)
+                .run()) {
+
+            OutputDestination target = context.getBean(OutputDestination.class);
+            Message<byte[]> sourceMessage = target.receive(10000);
+
+            final MessageConverter converter = context.getBean(CompositeMessageConverter.class);
+            UsageDetail usageDetail = (UsageDetail) converter
+                    .fromMessage(sourceMessage, UsageDetail.class);
+
+            assertThat(usageDetail.getUserId()).isBetween("user1", "user5");
+            assertThat(usageDetail.getData()).isBetween(0L, 700L);
+            assertThat(usageDetail.getDuration()).isBetween(0L, 300L);
+        }
     }
 
 }
